7dd626a168145020ce1d7a0e770096366fe9c174
Chameli <chame9dil@gmail.com>
2021-01-28 14:14:37 +0530

    table and chaies

--- 3DdiningRoom/Source.cpp
+++ 3DdiningRoom/Source.cpp
@@ -57,7 +57,7 @@ void tableLeg(float x1, float x2, float y1, float y2, float z1, float z2)
 
 	//Top
 	glBegin(GL_POLYGON);
-	//glColor4f(0.5f, 0.35f, 0.05f, 1.0f);
+	//glColor4f(1.0f, 0.5f, 0.5f, 1.0f);
 	glVertex3f(x2, y2, z1);
 	glVertex3f(x2, y2, z2);
 	glVertex3f(x1, y2, z2);
@@ -74,26 +74,18 @@ void tableLeg(float x1, float x2, float y1, float y2, float z1, float z2)
 	glEnd();
 }
 void table() {
-	glPushMatrix();
-	glRotatef(-30.0f, 1.0, 0.0, 0.0);
-	glRotatef(30.0f, 0.0, 1.0, 0.0);
-	glColor4f(0.5f, 0.35f, 0.05f, 1.0f);
-	tableLeg(0, 20, 0, 1, 0, 6);
+	
+	tableLeg(0, 10, 0, 1, 0, 20);
 
-	tableLeg(0.5, 1.5, -7.0, 0.0, 4.5, 5.5);
+	tableLeg(0.5, 1.5, -7.0, 0.0, 18.5, 19.5);
 	tableLeg(0.5, 1.5, -7.0, 0.0, 0.5, 1.5);
-	tableLeg(18.5, 19.5, -7.0, 0.0, 4.5, 5.5);
-	tableLeg(18.5, 19.5, -7.0, 0.0, 0.5, 1.5);
+	tableLeg(8.5, 9.5, -7.0, 0.0, 18.5, 19.5);
+	tableLeg(8.5, 9.5, -7.0, 0.0, 0.5, 1.5);
 
-	glPopMatrix();
 }
 
-void chair() {
-	glPushMatrix();
-	glRotatef(-30.0f, 1.0, 0.0, 0.0);
-	glRotatef(30.0f, 0.0, 1.0, 0.0);
-	glColor4f(0.5f, 0.35f, 0.05f, 1.0f);
-
+void chair() 
+{	
 	tableLeg(5.0, 10.0, -2.0, -2.4, 2.0, 7.5);
 	//legs of chair
 	tableLeg(5.0, 5.3, -2.3, -7.3, 2.0, 2.3);
@@ -104,22 +96,59 @@ void chair() {
 	//supporting
 	tableLeg(9.7, 10.0, -2.0, 7.0, 2, 2.3);
 	tableLeg(9.7, 10.0, -2.0, 7.0, 7.5, 7.8);
-	tableLeg(9.7, 10.0, 5.0, 7.0,2.0, 7.5);
+	tableLeg(9.7, 10.0, 5.0, 7.0,2.0, 7.5);	
+}
+void tableWithChair()
+{
+	glPushMatrix();
+	
+	glColor4f(0.5f, 0.35f, 0.05f, 1.0f);
+	table();
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(7,0,22);
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glRotatef(180.0f, 0.0, 1.0, 0.0);
+	chair();
+	glPopMatrix();
 
+	glPushMatrix();
+	glTranslatef(1, 0, 0);
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	chair();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(7, 0, 11);
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glRotatef(180.0f, 0.0, 1.0, 0.0);
+	chair();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(1, 0, 10);
+	glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
+	chair();
 	glPopMatrix();
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	table();
-	chair();
+	//table with 4 chairs
+	glPushMatrix();
+	glRotatef(-30.0f, 1.0, 0.0, 0.0);
+	glRotatef(30.0f, 0.0, 1.0, 0.0);
+	tableWithChair();
+	glPopMatrix();
+	
+	
 	glutSwapBuffers();
-
 }
 
-void changeSize(GLsizei w, GLsizei h) {
+void changeSize(GLsizei w, GLsizei h) 
+{
 	glViewport(0, 0, w, h);
 
 	GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
@@ -134,14 +163,11 @@ void changeSize(GLsizei w, GLsizei h) {
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-
 }
 
 /* Main function: GLUT runs as a console application starting at main()  */
-int main(int argc, char** argv) {
-
-
+int main(int argc, char** argv) 
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);       // Enable the Depht preception as well
 	glutInitWindowSize(720, 720);
